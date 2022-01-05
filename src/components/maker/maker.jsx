75b745cabbe4baa5325846c72dcc1cbdf4f1fae0
SpearJin <ckdwls2525@naver.com>
2022-01-05 14:50:03 +0900

    maker layout

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,12 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import Editor from '../editor/editor';
+
 import Preview from '../preview/preview';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './maker.module.css';
+import Editor from '../editor/editor';
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'ellie',
+      company: 'Samsung',
+      theme: 'light',
+      title: 'Software Engineer',
+      email: 'ckdwls2525@naver.com',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: 'ellie.png',
+    },
+    {
+      id: '2',
+      name: 'spearjin',
+      company: '기아',
+      theme: 'light',
+      title: 'Software Engineer',
+      email: 'spear2525@naver.com',
+      message: 'go for it',
+      fileName: 'spearjin',
+      fileURL: 'spearjin.png',
+    },
+    {
+      id: '3',
+      name: 'yumin',
+      company: 'LG',
+      theme: 'light',
+      title: 'Software Engineer',
+      email: 'yumin2525@naver.com',
+      message: 'go for it',
+      fileName: 'yumin',
+      fileURL: 'yumin.png',
+    },
+  ]);
+
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -19,12 +56,13 @@ const Maker = ({ authService }) => {
       }
     });
   });
+
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
